492187425cac2c5d3a6d5d180c4916d2c5780900
DageDarr <tianakristian15@gmail.com>
2025-10-21 14:27:54 +0300

    Фикс коммита

---
+++ app/models/film.py
@@ -0,0 +1,22 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.models.base import Base
+
+
+
+
+
+class Film(Base):
+    __tablename__ = 'films'
+
+    id : Mapped[int] = mapped_column(primary_key=True)
+    title : Mapped[str] = mapped_column(String(255), nullable=False)
+    description : Mapped[str] = mapped_column(String(2000), nullable=True)
+    year : Mapped[int] = mapped_column(nullable=False)
+    rating : Mapped[float] = mapped_column(nullable=True)
+
+
+    favorites: Mapped[list["Favorite"]] = relationship(back_populates="film")
+    watch_history : Mapped[list["WatchHistory"]] = relationship("film")
+
+
---
+++ app/models/user.py
@@ -0,0 +1,25 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column , relationship
+from app.models.base import Base
+
+
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id : Mapped[int] = mapped_column(primary_key=True)
+    username : Mapped[str] = mapped_column(String(50), nullable= False, unique=True)
+    email : Mapped[str] = mapped_column(String(255), nullable= False, unique=True)
+    hashed_password : Mapped[bytes] = mapped_column(nullable=False)
+
+
+    favorites : Mapped[list["Favorite"]] = relationship(back_populates="user")
+    watch_history : Mapped[list["WatchHistory"]] = relationship("user")
+    
+
+
+
+
+
