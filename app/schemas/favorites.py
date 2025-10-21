dda9ea467b7dd9d7149c441857938413f8d0a752
DageDarr <tianakristian15@gmail.com>
2025-10-21 23:53:57 +0300

    Добавление схем film, favorites, watch_history

---
+++ app/schemas/favorites.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, Field, ConfigDict
+
+
+
+
+
+class FavoriteBase(BaseModel):
+        
+    film_id : int = Field(..., gt=0, description='ID фильма в избраном')
+    user_id : int = Field(..., gt=0, description='ID юзера')
+
+
+class FavoriteCreate(FavoriteBase):
+    pass
+
+
+
+class FavoriteUpdate(BaseModel):
+    film_id : Optional[int] = Field(None, gt=0, description='ID фильма в избраном')
+
+
+class FavoriteResponse(FavoriteBase):
+    id : int = Field(..., description='ID избранного')
+    created_at : datetime = Field(..., description='Дата добавления в избраное фильма')
+
+    model_config = ConfigDict(from_attributes=True)
---
+++ app/schemas/film.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, Field, ConfigDict
+
+
+
+
+
+class FilmBase(BaseModel):
+    
+    title : str = Field(...,min_length=1, max_length=255, description='Название фильма')
+    description : Optional[str] = Field(None, max_length= 2000, description='Описание фильма')
+    year : int = Field(..., gt= 1900,le= datetime.now().year,description='Год выпуска фильма')
+    rating : float = Field(None, ge=0, le=10, description='Рейтинг фильма')
+
+
+
+class FilmCreate(FilmBase):
+    pass
+
+
+
+class FilmUpdate(BaseModel):
+    
+    title : Optional[str] = Field(None,min_length=1, max_length=255, description='Название фильма')
+    description : Optional[str] = Field(None, max_length= 2000, description='Описание фильма')
+    year : Optional[int] = Field(None, gt= 1900, description='Год выпуска фильма')
+    rating : Optional[float] = Field(None, gt=0, le=10, description='Рейтинг фильма')
+
+
+class FilmResponse(FilmBase):
+
+    id : int = Field(..., description='ID фильма')
+    created_at : Optional[datetime] = Field(None, description='Дата создания')
+
+    model_config = ConfigDict(from_attributes=True)
+
+
